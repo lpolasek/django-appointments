7da41a81e4f1fcc83114f791f01267d94e01a328
Luis Polasek <lpolasek@gmail.com>
2013-08-07 09:21:54 -0300

    Changed from django.shortcutsrender_to_response to django.shortcuts.render. Changed from django.http.HttpResponseRedirect to django.shortcuts.redirect. Use of django.core.urlresolvers.reverse in add and edit methods.

--- appointments/views.py
+++ appointments/views.py
@@ -1,7 +1,5 @@
 from django.core.urlresolvers import reverse
-from django.http import HttpResponse,HttpResponseRedirect
-from django.shortcuts import render_to_response, get_object_or_404
-from django.template import RequestContext
+from django.shortcuts import render, redirect, get_object_or_404
 from models import *
 
 def list(request,model,form):
@@ -9,10 +7,10 @@ def list(request,model,form):
 	empty_form = form()
 	items = model.objects.all()
 	template_name = '%s_list.html' % model_name
-	return render_to_response(
+	return render(
+		request,
 		template_name, 
 		{ 'items': items, 'form': empty_form, 'action': reverse('%s_add' % model_name), 'button_label': 'Add',  }, 
-		context_instance=RequestContext(request) 
 	)
 
 def add(request,model,form):
@@ -21,14 +19,14 @@ def add(request,model,form):
 		new_model = form(request.POST)
 		if new_model.is_valid():
 			new_model.save()
-			return HttpResponseRedirect(reverse(model_name))
+			return redirect(reverse(model_name))
 
 	items = model.objects.all()
 	template_name = '%s_list.html' % model_name
-	return render_to_response(
+	return render(
+		request,
 		template_name, 
-		{ 'items': items, 'form': new_model, 'action': 'add/', 'button_label': 'Add',  }, 
-		context_instance=RequestContext(request) 
+		{ 'items': items, 'form': new_model, 'action': reverse("%s_add" % model_name), 'button_label': 'Add',  }, 
 	)
 
 def delete(request,model,model_id):
@@ -36,7 +34,7 @@ def delete(request,model,model_id):
 	item = get_object_or_404(model, pk=model_id)
 	item.delete()
 	template_name = '%s_list.html' % model.__name__.lower()
-	return HttpResponseRedirect(reverse(model_name))
+	return redirect(reverse(model_name))
 
 def edit(request,model, form, model_id):
 	model_name = model.__name__.lower()
@@ -45,22 +43,22 @@ def edit(request,model, form, model_id):
 	if edit_form.is_valid():
 		item = edit_form.save()
 		item.save()
-		return HttpResponseRedirect(reverse(model_name))
+		return redirect(reverse(model_name))
 
 	items = model.objects.all()
 	template_name = '%s_list.html' % model_name
-	return render_to_response(
+	return render(
+		request,
 		template_name, 
-		{ 'items': items, 'form': edit_form, 'action': '', 'button_label': 'Update', 'model_id': model_id }, 
-		context_instance=RequestContext(request) 
+		{ 'items': items, 'form': edit_form, 'action': reverse("%s_edit" % model_name,  kwargs={ 'model_id': model_id }), 'button_label': 'Update'}, 
 	)
 
 def appointments_for_doctor(request, model_id, model):
 	item = get_object_or_404(model, pk=model_id)
 	items = item.appointment_set.all()
 	template_name = 'appointment_list.html'
-	return render_to_response(
+	return render(
+		request,
 		template_name, 
 		{ 'items': items, 'title': item.name }, 
-		context_instance=RequestContext(request) 
 	)
